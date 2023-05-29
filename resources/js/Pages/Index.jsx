2cc1c927cbd74bc5793c5d232406ec97bbc5d2e7
Tomislav Jozić <tommy@bb.agency>
2023-05-29 14:25:46 +0200

    configure index route for logged and not logged user

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,13 +1,13 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 
-export default function Dashboard({ auth }) {
+export default function Index({ auth }) {
     return (
         <AuthenticatedLayout
-            user={auth?.user}
+            user={auth.user}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Dashboard
+                    Index
                 </h2>
             }
         >
@@ -17,7 +17,7 @@ export default function Dashboard({ auth }) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            Show my question statuses and responses here.
+                            Index page - Show questions here
                         </div>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,11 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Index', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('index');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -35,4 +33,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
